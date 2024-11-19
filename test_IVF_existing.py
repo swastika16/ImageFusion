7844650fc335a4d9fc4f11c878dff3e1c665b14d
Swastika Dalmia <80032218+swastika16@users.noreply.github.com>
2024-11-19 12:56:14 +0530

    Add files via upload

---
+++ test_IVF_existing.py
@@ -0,0 +1,126 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+from net import Restormer_Encoder, Restormer_Decoder, BaseFeatureExtraction, Restormer_Encoder_Simplified, Restormer_Decoder_Simplified
+from utils.Evaluator import Evaluator
+from utils.img_read_save import img_save, image_read_cv2
+
+def remove_prefix(state_dict, prefix='module.'):
+    """Remove `prefix` from the keys of `state_dict`."""
+    new_state_dict = {}
+    for k, v in state_dict.items():
+        if k.startswith(prefix):
+            k = k[len(prefix):]
+        new_state_dict[k] = v
+    return new_state_dict
+
+# Device setup
+device = 'cpu'
+
+# Initialize models
+Encoder = Restormer_Encoder().to(device)
+Decoder = Restormer_Decoder().to(device)
+BaseFuseLayer = BaseFeatureExtraction(dim=64, num_heads=8).to(device)
+
+# Modify DetailFuseLayer to match the trained model
+class ModifiedDetailFeatureExtraction(nn.Module):
+    def __init__(self, num_layers=2):  # Use num_layers=2 as in training
+        super(ModifiedDetailFeatureExtraction, self).__init__()
+        self.layers = nn.ModuleList(
+            [nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1) for _ in range(num_layers)]
+        )
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = torch.relu(layer(x))
+        return x
+
+DetailFuseLayer = ModifiedDetailFeatureExtraction(num_layers=2).to(device)
+
+# Path to the checkpoint (update this to your actual checkpoint path)
+ckpt_path = "checkpoint"  # Adjust this path to where your model was saved during training
+
+# Load the checkpoint and adjust for `nn.DataParallel`
+checkpoint = {
+    'DIDF_Encoder': torch.load(os.path.join(ckpt_path, "DIDF_Encoder/encoder_epoch_0_1.pth"), map_location=device),
+    'DIDF_Decoder': torch.load(os.path.join(ckpt_path, "DIDF_Decoder/decoder_epoch_0_1.pth"), map_location=device),
+    'BaseFuseLayer': torch.load(os.path.join(ckpt_path, "BaseFuseLayer/BaseFuse_epoch_0_1.pth"), map_location=device),
+    'DetailFuseLayer': torch.load(os.path.join(ckpt_path, "DetailFuseLayer/DetailFuse_epoch_0_1.pth"), map_location=device)
+}
+
+# Remove 'module.' prefix if it exists (necessary if the model was trained with DataParallel)
+encoder_state_dict = remove_prefix(checkpoint['DIDF_Encoder'])
+decoder_state_dict = remove_prefix(checkpoint['DIDF_Decoder'])
+base_fuse_layer_state_dict = remove_prefix(checkpoint['BaseFuseLayer'])
+detail_fuse_layer_state_dict = remove_prefix(checkpoint['DetailFuseLayer'])
+
+# Load the model state dictionaries
+Encoder.load_state_dict(encoder_state_dict)
+Decoder.load_state_dict(decoder_state_dict)
+BaseFuseLayer.load_state_dict(base_fuse_layer_state_dict)
+DetailFuseLayer.load_state_dict(detail_fuse_layer_state_dict)
+
+# Set models to evaluation mode
+Encoder.eval()
+Decoder.eval()
+BaseFuseLayer.eval()
+DetailFuseLayer.eval()
+
+# Define the dataset names and paths
+for dataset_name in ["RoadScene", "TNO", "MRI_CT"]:
+    print("\n" * 2 + "=" * 80)
+    model_name = "CDDFuse"
+    print("The test result of " + dataset_name + ' :')
+
+    test_folder = os.path.join('test_img', dataset_name)
+    test_out_folder = os.path.join('test_result', dataset_name)
+
+    with torch.no_grad():
+        for img_name in os.listdir(os.path.join(test_folder, "ir")):
+            # Read and preprocess images
+            data_IR = image_read_cv2(os.path.join(test_folder, "ir", img_name), mode='GRAY')[
+                          np.newaxis, np.newaxis, ...] / 255.0
+            data_VIS = image_read_cv2(os.path.join(test_folder, "vi", img_name), mode='GRAY')[
+                           np.newaxis, np.newaxis, ...] / 255.0
+
+            data_IR, data_VIS = torch.FloatTensor(data_IR), torch.FloatTensor(data_VIS)
+            data_VIS, data_IR = data_VIS.to(device), data_IR.to(device)
+
+            # Forward pass through the network
+            feature_V_B, feature_V_D, feature_V = Encoder(data_VIS)
+            feature_I_B, feature_I_D, feature_I = Encoder(data_IR)
+            feature_F_B = BaseFuseLayer(feature_V_B + feature_I_B)
+            feature_F_D = DetailFuseLayer(feature_V_D + feature_I_D)
+            data_Fuse, _ = Decoder(data_VIS, feature_F_B, feature_F_D)
+            data_Fuse = (data_Fuse - torch.min(data_Fuse)) / (torch.max(data_Fuse) - torch.min(data_Fuse))
+            fi = np.squeeze((data_Fuse * 255).cpu().numpy())
+
+            # Save the fused image
+            img_save(fi, img_name.split(sep='.')[0], test_out_folder)
+
+    # Evaluate results
+    eval_folder = test_out_folder
+    ori_img_folder = test_folder
+
+    metric_result = np.zeros((8))
+    for img_name in os.listdir(os.path.join(ori_img_folder, "ir")):
+        ir = image_read_cv2(os.path.join(ori_img_folder, "ir", img_name), 'GRAY')
+        vi = image_read_cv2(os.path.join(ori_img_folder, "vi", img_name), 'GRAY')
+        fi = image_read_cv2(os.path.join(eval_folder, img_name.split('.')[0] + ".png"), 'GRAY')
+        metric_result += np.array([Evaluator.EN(fi), Evaluator.SD(fi), Evaluator.SF(fi), Evaluator.MI(fi, ir, vi),
+                                   Evaluator.SCD(fi, ir, vi), Evaluator.VIFF(fi, ir, vi), Evaluator.Qabf(fi, ir, vi),
+                                   Evaluator.SSIM(fi, ir, vi)])
+
+    metric_result /= len(os.listdir(eval_folder))
+    print("\t\t EN\t SD\t SF\t MI\tSCD\tVIF\tQabf\tSSIM")
+    print(model_name + '\t' + str(np.round(metric_result[0], 2)) + '\t'
+          + str(np.round(metric_result[1], 2)) + '\t'
+          + str(np.round(metric_result[2], 2)) + '\t'
+          + str(np.round(metric_result[3], 2)) + '\t'
+          + str(np.round(metric_result[4], 2)) + '\t'
+          + str(np.round(metric_result[5], 2)) + '\t'
+          + str(np.round(metric_result[6], 2)) + '\t'
+          + str(np.round(metric_result[7], 2))
+          )
+    print("=" * 80)
---
+++ train_existing_arch.py
@@ -0,0 +1,191 @@
+import os
+import sys
+import time
+import datetime
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+from utils.loss import Fusionloss
+import kornia
+from kornia.losses import SSIMLoss
+from net import Restormer_Encoder_Simplified, Restormer_Encoder, Restormer_Decoder, Restormer_Decoder_Simplified, BaseFeatureExtraction, ModifiedDetailFeatureExtraction
+from utils.dataset import H5Dataset
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+# Ensure necessary directories exist
+os.makedirs('checkpoint/DIDF_Encoder', exist_ok=True)
+os.makedirs('checkpoint/DIDF_Decoder', exist_ok=True)
+os.makedirs('checkpoint/BaseFuseLayer', exist_ok=True)
+os.makedirs('checkpoint/DetailFuseLayer', exist_ok=True)
+
+class SubsetH5Dataset(Dataset):
+    def __init__(self, dataset, indices):
+        self.dataset = dataset
+        self.indices = indices
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, idx):
+        return self.dataset[self.indices[idx]]
+
+# Configure network
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+criteria_fusion = Fusionloss()
+model_str = 'CDDFuse'
+
+# Set hyper-parameters for training
+num_epochs = 1
+epoch_gap = 2
+lr = 1e-4
+weight_decay = 0
+batch_size = 4
+GPU_number = os.environ['CUDA_VISIBLE_DEVICES']
+
+# Coefficients of the loss function
+coeff_mse_loss_VF = 1.
+coeff_mse_loss_IF = 1.
+coeff_decomp = 2.
+coeff_tv = 5.
+
+clip_grad_norm_value = 0.01
+optim_step = 20
+optim_gamma = 0.5
+
+# Model
+device = 'cpu'
+DIDF_Encoder = nn.DataParallel(Restormer_Encoder()).to(device)
+DIDF_Decoder = nn.DataParallel(Restormer_Decoder()).to(device)
+BaseFuseLayer = nn.DataParallel(BaseFeatureExtraction(dim=64, num_heads=8)).to(device)
+DetailFuseLayer = nn.DataParallel(ModifiedDetailFeatureExtraction(num_layers=2)).to(device)
+
+# Optimizer, scheduler and loss function
+optimizer1 = torch.optim.Adam(DIDF_Encoder.parameters(), lr=lr, weight_decay=weight_decay)
+optimizer2 = torch.optim.Adam(DIDF_Decoder.parameters(), lr=lr, weight_decay=weight_decay)
+optimizer3 = torch.optim.Adam(BaseFuseLayer.parameters(), lr=lr, weight_decay=weight_decay)
+optimizer4 = torch.optim.Adam(DetailFuseLayer.parameters(), lr=lr, weight_decay=weight_decay)
+
+scheduler1 = torch.optim.lr_scheduler.StepLR(optimizer1, step_size=optim_step, gamma=optim_gamma)
+scheduler2 = torch.optim.lr_scheduler.StepLR(optimizer2, step_size=optim_step, gamma=optim_gamma)
+scheduler3 = torch.optim.lr_scheduler.StepLR(optimizer3, step_size=optim_step, gamma=optim_gamma)
+scheduler4 = torch.optim.lr_scheduler.StepLR(optimizer4, step_size=optim_step, gamma=optim_gamma)
+
+MSELoss = nn.MSELoss()
+L1Loss = nn.L1Loss()
+Loss_ssim = SSIMLoss(window_size=11, reduction='mean')
+
+# Data loader
+num_samples = 50
+indices = list(range(num_samples))
+original_dataset = H5Dataset(r"data/MSRS_train_imgsize_128_stride_200.h5")
+subset_dataset = SubsetH5Dataset(original_dataset, indices)
+
+trainloader = DataLoader(subset_dataset,
+                         batch_size=batch_size,
+                         shuffle=True,
+                         num_workers=0)
+
+loader = {'train': trainloader}
+timestamp = datetime.datetime.now().strftime("%m-%d-%H-%M")
+
+# Train
+step = 0
+torch.backends.cudnn.benchmark = True
+prev_time = time.time()
+
+for epoch in range(num_epochs):
+    for i, (data_VIS, data_IR) in enumerate(loader['train']):
+        data_VIS, data_IR = data_VIS.to(device), data_IR.to(device)
+        DIDF_Encoder.train()
+        DIDF_Decoder.train()
+        BaseFuseLayer.train()
+        DetailFuseLayer.train()
+
+        DIDF_Encoder.zero_grad()
+        DIDF_Decoder.zero_grad()
+        BaseFuseLayer.zero_grad()
+        DetailFuseLayer.zero_grad()
+
+        optimizer1.zero_grad()
+        optimizer2.zero_grad()
+        optimizer3.zero_grad()
+        optimizer4.zero_grad()
+
+        if epoch < epoch_gap:  # Phase I
+            feature_V_B, feature_V_D, _ = DIDF_Encoder(data_VIS)
+            feature_I_B, feature_I_D, _ = DIDF_Encoder(data_IR)
+            data_VIS_hat, _ = DIDF_Decoder(data_VIS, feature_V_B, feature_V_D)
+            data_IR_hat, _ = DIDF_Decoder(data_IR, feature_I_B, feature_I_D)
+
+            mse_loss_V = 5 * Loss_ssim(data_VIS, data_VIS_hat) + MSELoss(data_VIS, data_VIS_hat)
+            mse_loss_I = 5 * Loss_ssim(data_IR, data_IR_hat) + MSELoss(data_IR, data_IR_hat)
+
+            Gradient_loss = L1Loss(kornia.filters.SpatialGradient()(data_VIS),
+                                   kornia.filters.SpatialGradient()(data_VIS_hat))
+
+            loss = coeff_mse_loss_VF * mse_loss_V + coeff_mse_loss_IF * mse_loss_I + coeff_tv * Gradient_loss
+
+        else:  # Phase II
+            feature_V = DIDF_Encoder(data_VIS)
+            feature_I = DIDF_Encoder(data_IR)
+            feature_F = BaseFuseLayer(feature_I + feature_V)
+            feature_F = DetailFuseLayer(feature_F)
+            data_Fuse = DIDF_Decoder(feature_F)
+
+            mse_loss_V = 5 * Loss_ssim(data_VIS, data_Fuse) + MSELoss(data_VIS, data_Fuse)
+            mse_loss_I = 5 * Loss_ssim(data_IR, data_Fuse) + MSELoss(data_IR, data_Fuse)
+
+            fusionloss, _, _ = criteria_fusion(data_VIS, data_IR, data_Fuse)
+
+            loss = fusionloss
+
+        loss.backward()
+        nn.utils.clip_grad_norm_(DIDF_Encoder.parameters(), max_norm=clip_grad_norm_value, norm_type=2)
+        nn.utils.clip_grad_norm_(DIDF_Decoder.parameters(), max_norm=clip_grad_norm_value, norm_type=2)
+        nn.utils.clip_grad_norm_(BaseFuseLayer.parameters(), max_norm=clip_grad_norm_value, norm_type=2)
+        nn.utils.clip_grad_norm_(DetailFuseLayer.parameters(), max_norm=clip_grad_norm_value, norm_type=2)
+        optimizer1.step()
+        optimizer2.step()
+        optimizer3.step()
+        optimizer4.step()
+
+        # Determine approximate time left
+        batches_done = epoch * len(loader['train']) + i
+        batches_left = num_epochs * len(loader['train']) - batches_done
+        time_left = datetime.timedelta(seconds=batches_left * (time.time() - prev_time))
+        prev_time = time.time()
+        sys.stdout.write(
+            "\r[Epoch %d/%d] [Batch %d/%d] [loss: %f] ETA: %.10s"
+            % (
+                epoch,
+                num_epochs,
+                i,
+                len(loader['train']),
+                loss.item(),
+                time_left,
+            )
+        )
+
+    # Adjust the learning rate
+    scheduler1.step()
+    scheduler2.step()
+    if not epoch < epoch_gap:
+        scheduler3.step()
+        scheduler4.step()
+
+    for optimizer in [optimizer1, optimizer2, optimizer3, optimizer4]:
+        if optimizer.param_groups[0]['lr'] <= 1e-6:
+            optimizer.param_groups[0]['lr'] = 1e-6
+
+    # Save model checkpoints
+    torch.save(DIDF_Encoder.state_dict(),
+               'checkpoint/DIDF_Encoder/encoder_epoch_%d_%d.pth' % (epoch, num_epochs))
+    torch.save(DIDF_Decoder.state_dict(),
+               'checkpoint/DIDF_Decoder/decoder_epoch_%d_%d.pth' % (epoch, num_epochs))
+    torch.save(BaseFuseLayer.state_dict(),
+               'checkpoint/BaseFuseLayer/BaseFuse_epoch_%d_%d.pth' % (epoch, num_epochs))
+    torch.save(DetailFuseLayer.state_dict(),
+               'checkpoint/DetailFuseLayer/DetailFuse_epoch_%d_%d.pth' % (epoch, num_epochs))
+
+print("\nTraining completed.")
